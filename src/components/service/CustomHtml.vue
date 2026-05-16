f6436d9d86ef91b89a204d59e806ea17099d8c94
R0GGER <r.meijer@hibbit.nl>
2026-05-17 00:45:07 +0200

    innerHTML issue for <script>

--- src/components/service/CustomHtml.vue
+++ src/components/service/CustomHtml.vue
@@ -2,7 +2,7 @@
   <div v-if="props.options?.hidden" class="hidden">
     <ServiceBase v-bind="props">
       <template #description="{ service }">
-        <span v-if="service?.data?.html" v-html="service.data.html" />
+        <span v-if="service?.data?.html" ref="hiddenHtmlRef" v-html="service.data.html" />
       </template>
     </ServiceBase>
   </div>
@@ -15,7 +15,7 @@
       {{ props.title || '' }}
     </template>
     <template #description="{ service }">
-      <span v-if="service?.data?.html" v-html="service.data.html" />
+      <span v-if="service?.data?.html" ref="visibleHtmlRef" v-html="service.data.html" />
     </template>
   </ServiceBase>
 </template>
@@ -25,6 +25,9 @@ import type { CustomHtmlService, ServiceClient } from '~/types'
 
 const props = defineProps<ServiceClient<CustomHtmlService>>()
 
+const hiddenHtmlRef = ref<HTMLElement | null>(null)
+const visibleHtmlRef = ref<HTMLElement | null>(null)
+
 const iconProps = computed(() => {
   if (!props.icon) {
     return {}
@@ -32,4 +35,29 @@ const iconProps = computed(() => {
   const { name: _, ...p } = props.icon
   return p
 })
+
+function activateScripts(container: HTMLElement) {
+  const scripts = container.querySelectorAll('script')
+  scripts.forEach((oldScript) => {
+    const newScript = document.createElement('script')
+    for (const attr of oldScript.attributes) {
+      newScript.setAttribute(attr.name, attr.value)
+    }
+    if (oldScript.textContent) {
+      newScript.textContent = oldScript.textContent
+    }
+    oldScript.parentNode?.replaceChild(newScript, oldScript)
+  })
+}
+
+function watchRef(elRef: Ref<HTMLElement | null>) {
+  watch(elRef, (el) => {
+    if (el) {
+      nextTick(() => activateScripts(el))
+    }
+  }, { immediate: true })
+}
+
+watchRef(hiddenHtmlRef)
+watchRef(visibleHtmlRef)
 </script>
